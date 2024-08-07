24be76ecdba461f59e1df27b960e613acd860bd9
aresada99 <ares.ada.dora@gmail.com>
2024-08-07 14:18:16 +0300

    added get images

--- Server/routes/dalleRoute.js
+++ Server/routes/dalleRoute.js
@@ -3,14 +3,14 @@ import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import ImageModel from "../models/imageModel.js"
 import { v2 as cloudinary } from 'cloudinary';
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 dotenv.config();
 
 const router = express.Router();
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
@@ -18,8 +18,13 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-router.route('/').get((req, res) => {
-    res.send('Test route of Dall-E Image Generation');
+router.route('/').get(async (req, res) => {
+    try {
+        const images = await ImageModel.find();
+        res.status(200).json(images);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.route('/').post(async (req, res) => {
@@ -40,18 +45,18 @@ router.route('/').post(async (req, res) => {
         const uniqueId = uuidv4();
 
         const uploadResult = await cloudinary.uploader
-        .upload(
-            response.data[0].url, {
+            .upload(
+                response.data[0].url, {
                 public_id: uniqueId,
             }
-        )
-        .catch((error) => {
-            console.log(error);
-        });
+            )
+            .catch((error) => {
+                console.log(error);
+            });
 
         const newImage = new ImageModel({
             prompt,
-            imgLink : uploadResult.url
+            imgLink: uploadResult.url
         });
 
         const createdImage = await newImage.save();
